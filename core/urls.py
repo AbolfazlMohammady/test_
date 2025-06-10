940559ffa9e7dd1f6958f4f7236b79cac617803c
= <sanare1382@gmail.com>
2025-06-10 18:41:41 +0330

    add refresh token

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import LoginRegesterApiView
+from .views import LoginRegesterApiView, RefreshTokenApi
 
 urlpatterns = [
     path('login/',LoginRegesterApiView.as_view()),
+    path('refresh/', RefreshTokenApi.as_view()),
 ]
--- core/views.py
+++ core/views.py
@@ -1,11 +1,17 @@
-from rest_framework import permissions ,status , viewsets , views
+from django.conf import settings
+
+from rest_framework import permissions ,status , viewsets 
 from rest_framework.response import Response
-from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken, AccessToken
+from rest_framework_simplejwt.exceptions import TokenError
 from .models import User
 from .serializers import UserSeializer
 
+import jwt
 
-class LoginRegesterApiView(views.APIView):
+
+class LoginRegesterApiView(APIView):
     def post(self , request):
         phone = request.data.get('phone')
         password = request.data.get('password')
@@ -45,3 +51,39 @@ class LoginRegesterApiView(views.APIView):
         }, status=status.HTTP_200_OK)
                         
 
+class RefreshTokenApi(APIView):
+    def post(self, request, format=None):
+        refresh_token = request.data.get('refresh')
+
+        if not refresh_token:
+            return Response(
+                {"error": "Refresh Token الزامی است"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        try:
+            decoded_token = jwt.decode(
+                refresh_token,
+                settings.SECRET_KEY,
+                algorithms=["HS256"]
+            )
+
+            user_id = decoded_token.get('user_id')
+
+            if not user_id:
+                raise TokenError("Invalid token structure")
+
+            user = User.objects.get(id=user_id)
+
+            access_token = AccessToken.for_user(user)
+
+            return Response(
+                {'access': str(access_token)},
+                status=status.HTTP_200_OK
+            )
+
+        except (jwt.ExpiredSignatureError, jwt.InvalidTokenError, User.DoesNotExist):
+            return Response(
+                {"error": "Invalid or expired refresh token"},
+                status=status.HTTP_401_UNAUTHORIZED
+            )
